577e33f998a4f8cdda7304bcf6d49ad82e42dd61
zhenerBY <zhener@tut.by>
2021-10-05 12:04:11 +0300

    first init

---
+++ Classes_for_dict.py
@@ -0,0 +1,184 @@
+from datetime import datetime, date, timedelta
+import json
+import os
+
+
+class Task:
+    counter = 0
+
+    def __init__(self, name: str = 'Test_task', description: str = 'Test_description',
+                 deadline=date.today().strftime('%Y-%m-%d'),
+                 created=date.today().strftime('%Y-%m-%d'), done=False):
+        self.name = name
+        self.description = description
+        self.created = created
+        self.deadline = deadline
+        self.done = done
+        self.counter = Task.counter
+        Task.counter += 1
+
+    @classmethod
+    def instances(cls):
+        return cls.counter + 1
+
+    @staticmethod
+    def att2create():
+        return [['name', 'Name'], ['description', 'Description'], ['deadline', 'deadline - "YYYY-MM-DD']]
+
+    @staticmethod
+    def att():
+        return ['name', 'description', 'created', 'deadline']
+
+    def attributes(self) -> list:
+        att = list(self.__dict__.keys())
+        att.remove('counter')
+        att.remove('done')
+        return att
+
+    def edit(self, name=None, description=None,
+             deadline=None,
+             created=None):
+        if name != None:
+            self.name = name
+        if description != None:
+            self.description = description
+        if deadline != None:
+            self.deadline = deadline
+        if created != None:
+            self.created = created
+
+
+class Tasks:
+
+    def __init__(self, user=None):
+        self.task_list = []
+        if user == None:
+            self.__user = 'Test_user'
+            self.task_list.append(Task())  # пустая задача
+        else:
+            self.__user = user
+            self.opentasks()
+
+    @staticmethod
+    def userslist() -> list:
+        files = os.listdir('./users/')
+        return [x[:-5] for x in files if x[-5:] == '.json']
+
+    @staticmethod
+    def adduser(name: str):
+        newuser = Task().__dict__
+        newuser.pop('counter')
+        try:
+            with open('./users/' + name + '.json', 'w') as f:
+                json.dump([newuser], f)
+        except FileNotFoundError:
+            os.mkdir('users')
+            with open('./users/' + name + '.json', 'w') as f:
+                json.dump([newuser], f)
+
+    @staticmethod
+    def deluser(name: str):
+        os.remove('./users/' + name + '.json')
+
+    @property
+    def user(self):
+        return self.__user
+
+    @user.setter
+    def user(self, name):
+        os.remove('./users/' + self.__user + '.json')
+        self.__user = name
+        self.safetasks()
+
+    def changeuser(self, name):
+        self.task_list = []
+        self.__user = name
+        self.opentasks()
+
+    def addtask(self, *args, **kwargs) -> None:
+        if args == () and kwargs == {}:
+            pass
+        else:
+            self.task_list.append(Task(*args, **kwargs))
+
+    def addlisttasks(self, tasks: list):
+        for el in tasks:
+            self.task_list.append(Task(**el))
+
+    def deltasks(self, *args: int) -> None:
+        ''' args this is Task counters
+        '''
+        for num, el in enumerate(self.task_list):
+            if el.counter in args:
+                self.task_list.pop(num)
+
+    def edittask(self, counter: int, **kwargs) -> None:
+        for i in self.task_list:
+            if i.counter == counter:
+                i.edit(**kwargs)
+
+    def showtasks(self, lbl=True) -> list:
+        tasklist = []
+        if lbl == True:
+            for task in self.task_list:
+                print(task.__dict__)
+        else:
+            for task in self.task_list:
+                tasklist.append(task.__dict__)
+            return tasklist
+
+    def shownames(self, *args) -> list:
+        ''' *args - counters
+        return list of names
+        '''
+        nameslist = []
+        for task in self.task_list:
+            if task.counter in args:
+                nameslist.append(task.name)
+        return nameslist
+
+    def tasks2save(self) -> list:
+        tasklist = []
+        for task in self.task_list:
+            tmpdict = {}
+            listtemp = list(task.__dict__.keys())
+            listtemp.remove('counter')
+            for task_2 in listtemp:
+                tmpdict[task_2] = task.__dict__[task_2]
+            tasklist.append(tmpdict)
+        return tasklist
+
+    def opentasks(self) -> None:
+        self.task_list = []
+        with open('./users/' + self.__user + '.json', 'r') as file_json:
+            self.addlisttasks(json.load(file_json))
+
+    def safetasks(self) -> None:
+        with open('./users/' + self.__user + '.json', 'w') as file_json:
+            json.dump(self.tasks2save(), file_json)
+
+    def sorted(self, attribute: str = 'name') -> list:
+        tasklist = []
+        [tasklist.append(x.__dict__) for x in self.task_list]
+        sorted_second = sorted(tasklist, key=lambda x: x['name'])  # second sort (by 'name')
+        return sorted(sorted_second, key=lambda x: x[attribute])
+
+    def taskdone(self, *args: int) -> None:
+        for el in self.task_list:
+            if el.counter in args:
+                el.done = True
+
+    def filter(self, text: str, key: str = '*') -> list:
+        '''Выводит отсортированный список
+        use key - '*' for search in all keys except "counter" and "done" '''
+        if key != '*':
+            return list(map(lambda x: x.__dict__,
+                            list(filter(lambda x: text.lower() in x.__dict__[key].lower(), self.task_list))))
+        else:
+            tasks = []
+            for i in Task.att():
+                tasks.append(set(filter(lambda x: text.lower() in x.__dict__[i].lower(), self.task_list)))
+            return list(map(lambda x: x.__dict__, list(tasks[0].union(*list(tasks[x] for x in range(1, len(tasks)))))))
+
+    def test(self):
+        print(self.task_list)
---
+++ TODOv0.2.py
@@ -0,0 +1,237 @@
+from Classes_for_dict import Task, Tasks
+import click
+
+
+def set_and_val(text: str, conditions: list = None, texterr: str = None, check=False, checkconditions=True):
+    """
+    text - start message text
+    texterr - error text
+    conditions - условия
+    check - подтверждение
+    checkconditions - checkconditions
+    """
+    if texterr == None:
+        texterr = 'Error!!! ' + text
+    while True:
+        value = input(f'{text}')
+        if checkconditions:
+            while value not in conditions:
+                value = input(f'{texterr}')
+        if check:
+            valuecheck = input(f'Your value is - {value}. Are you sure ? (Y)es/(N)o :')
+            if valuecheck in [*'Yy'] and valuecheck != '':
+                return value
+        else:
+            return value
+
+
+def table(func):
+    def wrapper(title: str, bottom_line: str, data):
+        length = 90
+        print(''.center(length, '-'))
+        print('|', (title).center(length - 2), '|', sep='')
+        print(''.center(length, '-'))
+        func(data)
+        print(''.center(length, '-'))
+        print('|', (bottom_line).center(length - 2), '|', sep='')
+        print(''.center(length, '-'))
+
+    return wrapper
+
+
+@table
+def show_list(lit: list):
+    for i in range(len(lit)):
+        print('|', str(i + 1).center(3), '- ', lit[i].ljust(38), sep='', end='')
+        if i % 2 == 1 and i != 0:
+            print(' |')
+    print()
+
+
+@table
+def show_dict(dct: dict):
+    for el in dct:
+        print('| ', el.ljust(15), ' - ', str(dct[el]).ljust(68), ' |', sep='')
+
+
+def user_select(name: str = None) -> str:
+    if name == None:
+        while True:
+            show_list('Users list', ' (a)dd User, (d)elete User     or     E(x)it ', Tasks.userslist())
+            action = set_and_val(
+                'Enter user number, or command :',
+                [*list(map(str, range(1, len(Tasks.userslist()) + 1))), *'aAdDxX'],
+            )
+            if action in 'aA':
+                if r_o:
+                    input('!!!Operation is prohibited!!! Press Enter')
+                else:
+                    name = set_and_val("Enter the name of the new user :", check=True, checkconditions=False)
+                    Tasks.adduser(name)
+            if action in 'dD':
+                if r_o:
+                    input('!!!Operation is prohibited!!! Press Enter')
+                else:
+                    name = set_and_val("Enter the name NUMBER to delete :",
+                                       [*list(map(str, range(1, len(Tasks.userslist()) + 1)))],
+                                       check=True)
+                    Tasks.deluser(Tasks.userslist()[int(name) - 1])
+            if action in 'xX':
+                exit()
+            if action in [*list(map(str, range(1, len(Tasks.userslist()) + 1)))]:
+                return Tasks.userslist()[int(action) - 1]
+    elif name in Tasks.userslist():
+        return name
+    else:
+        Tasks.adduser(name)
+        return name
+
+
+@table
+def show_tasks_table(lst: list) -> None:
+    print('|', '#'.center(3), '- ', sep='', end='')
+    print('Task name'.center(48), sep='', end='')
+    print('Created'.center(10), '  ', sep='', end='')
+    print('Deadline'.center(10), '  ', sep='', end='')
+    print('Status'.center(11), sep='', end='')
+    print('|')
+    for num, el in enumerate(lst):
+        if el['done']:
+            status = 'completed'
+        else:
+            status = 'in progress'
+        print('|', str(num + 1).center(3), '- ', sep='', end='')
+        print(el['name'][0:48].ljust(48), sep='', end='')
+        print(el['created'].ljust(10), '  ', sep='', end='')
+        print(el['deadline'].center(10), '  ', sep='', end='')
+        print(status.center(11), sep='', end='')
+        print('|')
+
+
+def show_tasks(lst: list) -> None:
+    show_tasks_table('Task list', 'You can do it all!', lst)
+
+
+def show_task_det(lst: list) -> None:
+    flag = True
+    while flag:
+        show_tasks_table('Task list', 'Choose one! or E(x)it', lst)
+        action = set_and_val('Enter task NUMBER :', list(map(str, [*range(1, len(lst) + 1), *'xX'])))
+        if action in 'xX':
+            break
+        for el in lst:
+            if lst[int(action) - 1]['counter'] == el['counter']:
+                show_dict(f'Task name - "{el["name"]}"', '', el)
+        action = input('Do you want to see another Task ? (Y)es or Enter :')
+        if action not in 'yY' or action == '':
+            flag = False
+
+
+def add_task(lst: list) -> None:
+    flag = True
+    while flag:
+        newtask = {}
+        for el in Task.att2create():
+            newtask[el[0]] = input(f'Enter {el[1]} :')
+        show_dict(f'Task name - "{newtask["name"]}"', '', newtask)
+        action = input('This is right? ? (Y)es or Enter :')
+        if action in 'yY':
+            flag = False
+    user.addtask(**newtask)
+
+
+def edit_task(lst: list) -> None:
+    show_tasks_table('Task list', 'Select a task to edit', lst)
+    action = set_and_val('Enter task NUMBER :', list(map(str, [*range(1, len(lst) + 1)])))
+    for el in lst:
+        if lst[int(action) - 1]['counter'] == el['counter']:
+            show_dict(f'Task name - "{el["name"]}"', '', el)
+            counter = el['counter']
+    key = set_and_val('Enter the key :', user.task_list[0].__dict__.keys())
+    value = set_and_val('Enter new text/value', checkconditions=False, check=True)
+    user.edittask(counter, **{key: value})
+
+
+def delete_task(lst: list) -> None:
+    show_tasks_table('Task list', 'Select a task to delete', lst)
+    nums = input('Enter task NUMBER or NUMBERS (num1,num2,...):')
+    nums = list(map(int, nums.replace(' ', '').split(sep=',')))
+    counters = []
+    for el in lst:
+        for i in nums:
+            if lst[int(i) - 1]['counter'] == el['counter']:
+                counters.append(el['counter'])
+    action = input(f'Do you want to delete {user.shownames(*counters)}? (Y)es or Enter')
+    if action in 'yY' and action != '':
+        user.deltasks(*counters)
+
+
+def search_task(lst: list) -> None:
+    text = input('Enter text to search for all keys or Enter to search for a single key :')
+    key = '*'
+    if text == '':
+        key = set_and_val(f'Enter key "{", ".join(user.task_list[0].__dict__.keys())}" :',
+                          user.task_list[0].__dict__.keys())
+        text = input('Enter text to search')
+    show_task_det(user.filter(text, key))
+
+
+def sort_task(lst: list) -> None:
+    key = set_and_val(f'Enter sorting key "{", ".join(user.task_list[0].__dict__.keys())}" :',
+                      user.task_list[0].__dict__.keys())
+    show_tasks_table(f'Sorted Task list by {key}', f'--{key}--', user.sorted(key))
+
+
+def done_task(lst: list) -> None:
+    show_tasks_table('Task list', 'Select a task to mark', lst)
+    nums = input('Enter task NUMBER or NUMBERS (num1,num2,...):')
+    nums = list(map(int, nums.replace(' ', '').split(sep=',')))
+    counters = []
+    for el in lst:
+        for i in nums:
+            if lst[int(i) - 1]['counter'] == el['counter']:
+                counters.append(el['counter'])
+    action = input(f'Do you want to mark {user.shownames(*counters)} as completed? (Y)es or Enter')
+    if action in 'yY' and action != '':
+        user.taskdone(*counters)
+
+
+@click.command()
+@click.option('-n', 'name', help="user's file name in ./users/ (without extension)")
+@click.option('-r/-w', 'r__o', default=False, help="r - for read-only mode")
+def main(name: str = None, r__o: bool = False):
+    global r_o
+    r_o = r__o
+    global user
+    user = Tasks(user_select(name))
+    while True:
+        show_list('LIST OF TASKS' + chr(174),
+                  ' L(O)GOUT - Change user    or     E(x)it ',
+                  [func_list[x][1] for x in func_list.keys()])
+        action = set_and_val('Enter num of operation :',
+                             [*func_list.keys(), *'xXoO'])
+        print(action)
+        if action in 'xX':
+            if not r_o:
+                user.safetasks()
+            exit()
+        if action in 'oO':
+            if not r_o:
+                user.safetasks()
+            user = Tasks(user_select())
+        if action in func_list.keys():
+            func_list[action][0](user.showtasks(False))
+        input('Press Enter to continue')
+
+
+func_list = {'1': (show_tasks, 'Show tasks list'),
+             '2': (show_task_det, 'Show task details'),
+             '3': (add_task, 'Add task'),
+             '4': (edit_task, 'Edit task'),
+             '5': (delete_task, 'Delete task'),
+             '6': (search_task, 'Search for a task'),
+             '7': (sort_task, 'Display sorted list'),
+             '8': (done_task, 'Mark task as completed'),
+             }
+
+main()
